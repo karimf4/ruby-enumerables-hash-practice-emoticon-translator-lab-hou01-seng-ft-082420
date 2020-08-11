7112d30e65a050e91c6a1d55cc426e841115f96c
Farhanul Karim <farhanulkarim3770@gmail.com>
2020-08-11 03:05:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,30 +1,31 @@
 # require modules here
 
 require 'yaml'
-require 'pry'
 
-def load_library(path)
-  # code goes here
-  get_emoticon = {}
-  get_meaning = {}
-  YAML.load_file(path).each do |k, v|
-    get_meaning[v[1]] = k
-    get_emoticon[v[0]] = v[1]
+ENGLISH = 0
+JAPANESE = 1
+
+def load_library(filepath)
+  library_hash = YAML.load_file(filepath)
+  translator = {"get_meaning" => {},
+               "get_emoticon" => {}}
+
+  library_hash.each do |english_meaning, emoticons_list|
+    translator["get_meaning"][emoticons_list[JAPANESE]] = english_meaning
+    translator["get_emoticon"][emoticons_list[ENGLISH]] = emoticons_list[JAPANESE]
   end
-  result = {}
-  result["get_meaning"] = get_meaning
-  result["get_emoticon"] = get_emoticon
-  result
+
+  translator
 end
 
-def get_japanese_emoticon(path, emo)
-  # code goes here
-  dictionary = load_library(path)
-  dictionary['get_emoticon'][emo] ? dictionary['get_emoticon'][emo] : "Sorry, that emoticon was not found"
+def get_japanese_emoticon(filepath, emoticon)
+  translator = load_library(filepath)
+  translated = translator["get_emoticon"][emoticon]
+  translated ? translated : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning(path, emo)
-  # code goes here
-  dictionary = load_library(path)
-  dictionary['get_meaning'][emo] ? dictionary['get_meaning'][emo] : "Sorry, that emoticon was not found"
+def get_english_meaning(filepath, emoticon)
+  translator = load_library(filepath)
+  meaning = translator["get_meaning"][emoticon]
+  meaning ? meaning : "Sorry, that emoticon was not found"
 end
